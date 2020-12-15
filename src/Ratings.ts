a8ff145236f024360428ddd7b94ac29a621b6319
Jose A. Alvarado <megaalpha200@gmail.com>
2020-12-14 22:51:08 -0600

    Updated Packages

--- src/Ratings.ts
+++ src/Ratings.ts
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/js/bootstrap';
 import RatingSection, { CommentRes } from './components/RatingSection';
 import RatingsSummary, { defaultRatingPercentages } from './components/RatingsSummary';
